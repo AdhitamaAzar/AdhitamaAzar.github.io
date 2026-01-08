7edf07d5a986fab77a505d57ce07d4193d22cf05
AdhitamaAzar <adhitamagaming969@gmail.com>
2026-01-08 12:30:29 +0700

    datavase

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+      const scriptURL = 'https://script.google.com/macros/s/AKfycby8fbIPeQTXsF7QOVfgQlqNWXkloX8wt2nTrmaf4GnS403qNVfxXy7q-ygfWv5JSl82/exec';
+      const form = document.forms['Database-Portfolio'];
+      const btnKirim = document.querySelector('.btn-kirim');
+      const btnLoading = document.querySelector('.btn-loading');
+      const myAlert = document.querySelector('.my-alert');
+
+      form.addEventListener('submit', e => {
+        e.preventDefault();
+        btnLoading.classList.remove('d-none');
+        btnKirim.classList.add('d-none');
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+            btnLoading.classList.add('d-none');
+            btnKirim.classList.remove('d-none');
+            myAlert.style.display = 'block';
+            form.reset();
+            setTimeout(() => myAlert.style.display = 'none', 5000);
+          })
+          .catch(error => console.error('Error!', error.message));
+      });
